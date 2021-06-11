044adc22d030636b2406ba12e47933da5b3c3bb3
kis yad <kishor_ras@yahoo.com>
2021-06-12 02:34:26 +0530

    Welcome Screen and Credits

---
+++ Simulator/Assets/welcomeScreen.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class welcomeScreen : MonoBehaviour
+{
+   public void LoadManualFlight ()
+   {
+   	SceneManager.LoadScene("ManualSwarm");
+
+   }
+   public void LoadAutoSwarm()
+   {
+   	SceneManager.LoadScene("AutoSwarm");
+
+   }
+    public void LoadFollowTheLeader()
+   {
+   	SceneManager.LoadScene("FollowLeaderSwarm");
+
+   }
+     public void IndipendentPath()
+   {
+    SceneManager.LoadScene("navScene 2");
+
+   }
+    public void SinglePath()
+   {
+   	SceneManager.LoadScene("NavSceneCity");
+
+   }
+    public void CreditsScreen()
+   {
+   	SceneManager.LoadScene("CreditsScreen");
+
+   }
+   public void CircuitMovement()
+   {
+   	SceneManager.LoadScene("1.5_Circuit_In_Environment");
+
+   }
+    public void WeatherEffects()
+   {
+   	SceneManager.LoadScene("WeatherEffects");
+
+   }
+    public void Credits()
+   {
+   	SceneManager.LoadScene("CreditsScreen");
+
+   }
+
+}
