e18902c3ab4ead5804f9918258b220a3b4cec91b
kis yad <kishor_ras@yahoo.com>
2021-06-14 03:05:23 +0530

    camera added to drone, Display output of camera on Rawimage on screen

---
+++ Simulator/Assets/CamToRawImage.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CamToRawImage : MonoBehaviour
+{
+    public GameObject[] cameras;
+    public RenderTexture guiCameraRenderTexture;
+ 
+    // Start is called before the first frame update
+    void Start()
+    {
+       
+    }
+ 
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.Escape))
+        {
+            var cam0 = cameras[0].GetComponent<Camera>();
+            cam0.targetTexture = guiCameraRenderTexture;
+           
+            cameras[1].SetActive(true);
+        }
+    }
+}
